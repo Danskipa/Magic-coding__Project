6d79dddeb2ac9de9b86691a6fd113100cca3aaf0
Andrew stecenko <andrew1stecenko@gmail.com>
2026-05-10 11:51:23 +0200

    add game data and rendering functionality; update HTML structure

---
+++ src/js/games/test_game.js
@@ -0,0 +1,8 @@
+// export const initTestGame = () => {
+//   const gameContainer = document.querySelector('.game-id_1');
+
+//   const gameHTML = '<h2>test game</h2>';
+//   gameContainer.innerHTML = gameHTML;
+
+//   //logic
+// };
