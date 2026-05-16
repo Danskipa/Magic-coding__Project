52406b4ae475220e465e5fd73a4fa1a8b9c55b95
Filip Litvin <litvinfilip5@gmail.com>
2026-05-16 22:32:37 +0200

    випавив помилки guess-number

--- src/js/games/guess-number.js
+++ src/js/games/guess-number.js
@@ -1,18 +1,46 @@
-let secretNumber = Math.floor(Math.random() * 100) + 1;
+const initGuessNumberGame = () => {
+  const container = document.querySelector('.game-id_2');
 
-const input = document.querySelector('.guess-input');
-const subtitle = document.querySelector('.guess-subtitle');
+  const html = `
+    <section class="guess-number">
+      <div class="container">
+        <h2 class="guess-title">Вгадай число, яке загадав комп’ютер</h2>
+        <div class="guess-box">
+          <div class="guess-input-wrapper">
+            <input
+              class="guess-input"
+              placeholder="Введіть число"
+              id="number"
+              type="number"
+            />
+          </div>
+          <label for="number"></label>
+          <h3 class="guess-subtitle"></h3>
+        </div>
+      </div>
+    </section>
+  `;
 
-subtitle.textContent = '';
+  container.innerHTML = html;
 
-const handleGuess = event => {
-  const userValue = Number(event.target.value);
-  if (userValue === secretNumber) {
-    subtitle.textContent = `Вітаю, ви вгадали число! (${secretNumber})`;
-  } else if (userValue > secretNumber) {
-    subtitle.textContent = 'Занадто велике число';
-  } else {
-    subtitle.textContent = 'Занадто мале число';
-  }
+  const input = container.querySelector('.guess-input');
+  const subtitle = container.querySelector('.guess-subtitle');
+
+  const secretNumber = Math.floor(Math.random() * 100) + 1;
+
+  const handleGuess = event => {
+    const value = Number(event.target.value);
+
+    if (value === secretNumber) {
+      subtitle.textContent = `Вітаю, ви вгадали число! (${secretNumber})`;
+    } else if (value > secretNumber) {
+      subtitle.textContent = 'Занадто велике число';
+    } else {
+      subtitle.textContent = 'Занадто мале число';
+    }
+  };
+
+  input.addEventListener('input', handleGuess);
 };
-input.addEventListener('change', handleGuess);
+
+document.addEventListener('DOMContentLoaded', initGuessNumberGame);
